f34f31928ad151a5034838eeaca953bdce93e5b8
Sky2High <Sky2High@protonmail.com>
2022-03-03 14:11:30 -0400

    Previous commit crashed due to core module initialization error, missed 'http://' in settings.py

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
 
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -170,5 +170,5 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
 "http://localhost:3000",
 "http://127.0.0.1:3000",
-'feh-resplendent.herokuapp.com:3000'
+'https://feh-resplendent.herokuapp.com:3000'
 ]
